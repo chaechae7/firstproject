0f3a46997cc651b3305878e31976cda20170b261
ysj13 <ysj13@kakao.com>
2020-06-22 13:25:19 +0900

    0622 1:25

--- project/src/main/java/com/kh/spring/board/controller/boardController.java
+++ project/src/main/java/com/kh/spring/board/controller/boardController.java
@@ -1,8 +1,36 @@
 package com.kh.spring.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.kh.spring.board.model.service.boardService;
+import com.kh.spring.board.model.vo.Board;
 
 @Controller
+@RequestMapping("/board")
 public class boardController {
-
+	
+	//게시글 등록 페이지로 이동
+	@RequestMapping(value="/write", method=RequestMethod.GET)
+	
+	public String writeGET() {
+		
+		return "/board/write";
+		
+	}
+	
+	//게시글 등록 처리
+	@RequestMapping(value = "/write", method=RequestMethod.POST)
+	
+	public String writePOST(Board board, RedirectAttributes redirectAttributes) {
+		
+		
+		redirectAttributes.addFlashAttribute("msg", "게시글 등록 성공!");
+		
+		return "redirect:/board/list";
+	}
+	
+	
 }
