f86832e0b90fb17873aca695ac38506469552ab6
chaechae7 <59869409+chaechae7@users.noreply.github.com>
2020-06-26 18:12:29 +0900

    20.06.26 회원가입 구현 (암호 복호화 제외)
    회원가입 구현
    암호 복호화 제외(추가해야되는 사항)

--- project/src/main/java/com/kh/spring/member/controller/MemberController.java
+++ project/src/main/java/com/kh/spring/member/controller/MemberController.java
@@ -1,15 +1,22 @@
 package com.kh.spring.member.controller;
 
 
+import org.apache.jasper.tagplugins.jstl.core.Redirect;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.kh.spring.member.model.service.MemberService;
 import com.kh.spring.member.model.vo.Member;
 
-@RestController
+@Controller
 @RequestMapping("/member")
 public class MemberController {
 	
@@ -17,6 +24,7 @@ public class MemberController {
 	private MemberService memberService;
 
 	//회원가입요청
+	@PostMapping("/enroll.do")
 	public String enroll(Member member, RedirectAttributes redirectAttributes) {
 		
 		//비밀번호 암호화
@@ -29,13 +37,13 @@ public class MemberController {
 		int result = memberService.insertMember(member);
 		
 		String msg = "";
-		if(result>0) msg = "회원가입 성공하셨습니다.";
-		else msg = "회원가입 실패!";
+		if(result>0) msg = "회원가입에 성공하셨습니다!";
+		else msg = "회원가입에 실패하셨습니다.";
 		
-		//메세지와함께 리다이렉트
-		redirectAttributes.addFlashAttribute("msg", msg);
-	
-		return "redirect:/";
+		
+		 redirectAttributes.addFlashAttribute("msg", msg);
+		
+		 return "redirect:/";
 	}
 	
 	//myPageMain으로 이동
